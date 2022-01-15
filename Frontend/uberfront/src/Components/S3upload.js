374b1c55da879c0a8a89435a9647295b24eb4993
AbhiramAtluri <90342768+AbhiramAtluri@users.noreply.github.com>
2022-01-15 12:57:59 -0800

    Update S3upload.js

--- Frontend/uberfront/src/Components/S3upload.js
+++ Frontend/uberfront/src/Components/S3upload.js
@@ -2,17 +2,17 @@ import S3 from 'react-aws-s3';
 
 
 
-const config = {
-    bucketName: 'uberbucket98',
-    dirName: 'media',
-    region: 'us-east-2',
-    accessKeyId: 'AKIAZO3UJQCCTCHR6FPV',
-    secretAccessKey: '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',   
-}
+// const config = {
+//     bucketName: 'uberbucket98',
+//     dirName: 'media',
+//     region: 'us-east-2',
+//     accessKeyId
+//     secretAccessKey: ,   
+// }
 
 
 
-export default config
+// export default config
 
 
 
